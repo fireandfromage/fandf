b23cb244e67f9dbec9b657f0ab399cba015bd3b1
Shaws <seanrichardparker@gmail.com>
2018-11-05 07:28:36 -0500

    implementing React-Router, disabling some styles

---
+++ fandf/src/components/Catering.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+export default class Catering extends Component {
+  render() {
+    return (
+      <section id="catering" style={{paddingTop:'120px'}}>
+        <h1>Raclette Caterer in Provo, UT.</h1>
+
+        <p>
+          The main focus of Fire and Fromage is to provide an excellent culinary
+          experience without sacrificing tradition.
+        </p>
+
+        <p>We provide various types of catered dinners.</p>
+
+        <ul>
+          <li>Plated Raclette Dinners</li>
+          <li>Buffet Raclette Dinners</li>
+          <li>Small Scale Raclette Parties ( For Families and Friends)</li>
+          <li>Fondue Parties</li>
+          <li>Cheese Tastings</li>
+        </ul>
+
+        <p>
+          Also as a professionally trained Chef, Degen can really cook anything
+          you would like for a catered event.
+        </p>
+
+        <p>Call us today for your custom quote!</p>
+      </section>
+    );
+  }
+}
---
+++ fandf/src/components/Home.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+export default class extends Component {
+  render() {
+    return (
+      <section id="home" style={{ paddingTop: "120px" }}>
+        <h1>Raclette, The Swiss Tradition</h1>
+        <p>
+          Raclette is a traditional dish of cheese melted on the wheel or block,
+          that is then scraped over bread, potatoes and pickles.
+        </p>
+
+        <p>
+          This was the traditional dish of Swiss herdsman, when they would take
+          their cows up into the high summer pastures of the Alps. They would
+          take the cheese, that was made especially for melting, and
+          non-perishable items like pickles and potatoes. After a hard days work
+          they would build a fire and melt the cheese on the block, scraping it
+          over their food.
+        </p>
+
+        <p>
+          At Fire and Fromage, we seek to bring this wonderful tradition to Utah
+          valley. We present our lovely Raclette with tradition in mind, but
+          adding our own new twists.
+        </p>
+
+        <p>Give us a call to set up your event today!</p>
+      </section>
+    );
+  }
+}
