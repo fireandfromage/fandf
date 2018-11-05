af38a4482fcdd18dec185db6e0e546072983ad95
Fire and Fromage <39216218+fireandfromage@users.noreply.github.com>
2018-11-05 07:54:54 -0500

    Revert "adding content and sticky nav"

--- nav.php
+++
@@ -1,9 +0,0 @@
-  <nav style="position:fixed; top:0; width:100%; background:white; border-bottom:1px solid black;">
-    <ul>
-      <li><a href="#home">Home</a></li>
-      <li><a href="#about">About Us</a></li>
-      <li><a href="#catering">Catering</a></li>
-      <li><a href="#booth">Food Booth</a></li>
-      <li><a href="#contact">Contact Us</a></li>
-    </ul>
-  </nav>
