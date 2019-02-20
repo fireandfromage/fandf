3075a617c8d5656ea897a240fa9412e0a0c03f98
Shaws <seanrichardparker@gmail.com>
2019-02-20 09:24:04 -0500

    setting up Switch and adding 404 page

--- fandf/src/components/Content/Content.jsx
+++ fandf/src/components/Content/Content.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./Home";
 import About from "./About";
 import Catering from "./Catering";
 import FoodBooth from "./FoodBooth";
 import Contact from "./Contact";
+import NoMatch from "./NoMatch";
 
 import "./content.scss";
 
@@ -13,11 +14,16 @@ export default class Content extends Component {
   render() {
     return (
       <main className="content">
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/catering" component={Catering} />
-        <Route exact path="/booth" component={FoodBooth} />
-        <Route exact path="/contact" component={Contact} />
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/catering" component={Catering} />
+            <Route exact path="/booth" component={FoodBooth} />
+            <Route exact path="/contact" component={Contact} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
       </main>
     );
   }
