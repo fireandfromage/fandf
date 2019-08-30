52ad0c683baa076a6ec737dc970c7fd81616829e
Shaws <seanrichardparker@gmail.com>
2019-08-30 08:18:14 -0400

    Updates

---
+++ precache-manifest.1a3b67323c4f92fcaccaa89b250514a1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e217e8436cb172209ff711626b625e2",
+    "url": "/fireandfromage/static/media/pewter_with_board.7e217e84.jpg"
+  },
+  {
+    "revision": "e7f37f699ab86ac401fdc96683321198",
+    "url": "/fireandfromage/static/media/chef_degen.e7f37f69.jpg"
+  },
+  {
+    "revision": "7942ac2b21de68a787865e2efc55c937",
+    "url": "/fireandfromage/static/media/booth.7942ac2b.jpg"
+  },
+  {
+    "revision": "aad067f4214f82f2f451bc7937aacefa",
+    "url": "/fireandfromage/static/media/alps_horn.aad067f4.jpg"
+  },
+  {
+    "revision": "aff162db93ebad06ca7f",
+    "url": "/fireandfromage/static/js/runtime~main.aff162db.js"
+  },
+  {
+    "revision": "af86c13492899dfa8466",
+    "url": "/fireandfromage/static/js/main.af86c134.chunk.js"
+  },
+  {
+    "revision": "6f5fe9b929971c4143b6",
+    "url": "/fireandfromage/static/js/1.6f5fe9b9.chunk.js"
+  },
+  {
+    "revision": "af86c13492899dfa8466",
+    "url": "/fireandfromage/static/css/main.e1800a0d.chunk.css"
+  },
+  {
+    "revision": "807350e5fad4be59fd90fb906cb05fdb",
+    "url": "/fireandfromage/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d991d0af4ca9696f105f477ddf048a13.js"
+  "/fireandfromage/precache-manifest.1a3b67323c4f92fcaccaa89b250514a1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/fireandfromage/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
