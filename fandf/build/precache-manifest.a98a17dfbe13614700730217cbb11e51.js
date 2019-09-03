fd134939b06d51a5f9b749cb71872b32dec3cc22
Shaws <seanrichardparker@gmail.com>
2019-09-03 06:41:28 -0400

    correct filepaths after deploy

---
+++ fandf/build/precache-manifest.a98a17dfbe13614700730217cbb11e51.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e217e8436cb172209ff711626b625e2",
+    "url": "/kingofcheese/fandf/static/media/pewter_with_board.7e217e84.jpg"
+  },
+  {
+    "revision": "e7f37f699ab86ac401fdc96683321198",
+    "url": "/kingofcheese/fandf/static/media/chef_degen.e7f37f69.jpg"
+  },
+  {
+    "revision": "7942ac2b21de68a787865e2efc55c937",
+    "url": "/kingofcheese/fandf/static/media/booth.7942ac2b.jpg"
+  },
+  {
+    "revision": "aad067f4214f82f2f451bc7937aacefa",
+    "url": "/kingofcheese/fandf/static/media/alps_horn.aad067f4.jpg"
+  },
+  {
+    "revision": "609f81492dcaa849a1c3",
+    "url": "/kingofcheese/fandf/static/js/runtime~main.609f8149.js"
+  },
+  {
+    "revision": "47fedad0be74a51aeacf",
+    "url": "/kingofcheese/fandf/static/js/main.47fedad0.chunk.js"
+  },
+  {
+    "revision": "6f5fe9b929971c4143b6",
+    "url": "/kingofcheese/fandf/static/js/1.6f5fe9b9.chunk.js"
+  },
+  {
+    "revision": "47fedad0be74a51aeacf",
+    "url": "/kingofcheese/fandf/static/css/main.9688bed2.chunk.css"
+  },
+  {
+    "revision": "d4146f1b807eea594653e7cc0bcbb762",
+    "url": "/kingofcheese/fandf/index.html"
+  }
+];
