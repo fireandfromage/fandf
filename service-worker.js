413e898b84ea5fb84f78395fb6905cafba75451b
Shaws <seanrichardparker@gmail.com>
2019-09-05 14:56:03 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fireandfromage/precache-manifest.1a3b67323c4f92fcaccaa89b250514a1.js"
+  "/kingofcheese/precache-manifest.612b41e84f8419e338e455673b1c602a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/fireandfromage/index.html", {
+workbox.routing.registerNavigationRoute("/kingofcheese/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
