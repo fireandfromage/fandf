6fe1706e6de9455b257c2fb1bc13bb7a012b8eae
Shaws <seanrichardparker@gmail.com>
2019-09-06 08:04:00 -0400

    remove build folder from gitignore

---
+++ build/precache-manifest.89e401d62904d930784e5ef15b31223a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e217e8436cb172209ff711626b625e2",
+    "url": "/static/media/pewter_with_board.7e217e84.jpg"
+  },
+  {
+    "revision": "e7f37f699ab86ac401fdc96683321198",
+    "url": "/static/media/chef_degen.e7f37f69.jpg"
+  },
+  {
+    "revision": "7942ac2b21de68a787865e2efc55c937",
+    "url": "/static/media/booth.7942ac2b.jpg"
+  },
+  {
+    "revision": "aad067f4214f82f2f451bc7937aacefa",
+    "url": "/static/media/alps_horn.aad067f4.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "32d127fc2fef9f98d2a7",
+    "url": "/static/js/main.32d127fc.chunk.js"
+  },
+  {
+    "revision": "6f5fe9b929971c4143b6",
+    "url": "/static/js/1.6f5fe9b9.chunk.js"
+  },
+  {
+    "revision": "32d127fc2fef9f98d2a7",
+    "url": "/static/css/main.d04b9cb6.chunk.css"
+  },
+  {
+    "revision": "918cb4618e12171bd81cf3e93358b65c",
+    "url": "/index.html"
+  }
+];
